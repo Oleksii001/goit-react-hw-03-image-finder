10dd45cd09c70aee72d4a09752c5e6765f442291
Oleksii001 <oleksii.burlaka@gmail.com>
2023-07-31 13:13:13 +0300

    finish task

---
+++ src/services/PixabayApi.jsx
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+const API_KEY = '37263308-2603284e2849c610a8b3f752c';
+
+export const perPage = 12;
+
+export const getImages = async (query, page) => {
+  const response = await axios.get(
+    `?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
+  return response.data;
+};
+
+export const normalizedImages = imagesArray =>
+  imagesArray.map(({ id, tags, webformatURL, largeImageURL }) => {
+    return { id, tags, webformatURL, largeImageURL };
+  });
